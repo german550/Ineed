acb3554ddccbc84a8798e08c98ec9bb7150c06d3
GERMAN GARZON <germangarzon@GERMANs-MacBook-Air.local>
2020-02-12 15:52:57 -0500

    projectsControllers

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,35 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+    @projects = Project.all.order("created_at DESC")
+  end
+  def new
+    @project = Project.new
+  end
+  def create
+    @project = Project.new(project_params)
+    @project.user_id =  current_user.id
+    @project.save
+      redirect_to @project
+  end
+  def show
+    @project = Project.find(params[:id])
+  end
+  def destroy
+      project = Project.find(params[:id])
+      if current_user == project.user
+        project.destroy
+        redirect_to "/projects/"
+        flash[:notice] = "project deleted"
+      else
+        redirect_back(fallback_location)
+        root_path
+        flash[:alert] = "Not authorized to delete this post"
+      end
+    end
+
+  private
+    def project_params
+        params.require(:project).permit(:description, :image)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-  # get "/users", to: "home#users"
-  # get "/sign_in", to: "home#sign_in"
-  # get "/sign_out", to: "home#sign_out"
+  resources :projects
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
